6483fd1ae8349782e0346c5f78a35080a27fedc9
evelo <evelo0702@gmail.com>
2025-03-13 00:47:37 +0900

    update: 설명용도의 옵션 태그 disabled 처리

--- src/components/DataFilter.tsx
+++ src/components/DataFilter.tsx
@@ -32,7 +32,9 @@ export default function DataFilter({
           value={selectedType === "month" ? selectedDate : ""}
           onChange={(e) => handleMonthChange(e.target.value)}
         >
-          <option value="">월별 사용량 보기</option>
+          <option value="" disabled>
+            월별 사용량 보기
+          </option>
           {filters.month.map((i) => (
             <option key={i} value={i}>
               {i}
@@ -46,16 +48,15 @@ export default function DataFilter({
           value={selectedType === "year" ? selectedDate : ""}
           onChange={(e) => handleYearChange(e.target.value)}
         >
-          <option value="">연도별 사용량 보기</option>
+          <option value="" disabled>
+            연도별 사용량 보기
+          </option>
           {filters.year.map((i) => (
             <option key={i} value={i}>
               {i}
             </option>
           ))}
         </select>
-        <button className="px-4 py-2 bg-black text-white dark:bg-white dark:text-black font-medium rounded-lg border border-neutral-300 dark:border-neutral-600 hover:bg-neutral-800 hover:text-white dark:hover:bg-neutral-200 dark:hover:text-black">
-          사용량 보기
-        </button>
       </div>
     </div>
   );
